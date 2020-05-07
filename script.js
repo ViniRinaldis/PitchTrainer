3b9a3d8ef81c65bab4c1c4260b9666bf12569a73
Vini Reato <vinireato@gmail.com>
2020-05-06 22:42:41 -0300

    failing streak and trys array

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C_above'];
+var trys = [];
 var played_note = '';
 var noteStreak = 0;
+var failingStreak = 0;
 
 function play_note(note){
   var audio = new Audio('notes/'+ note +'.mp3');
@@ -24,14 +26,18 @@ function verifyNote(note) {
 }
 
 function right_note(){
+  trys.push([failingStreak, played_note])
   var result = document.getElementById("result");
   result.innerHTML = "Sucesso!";
   result.classList.remove("failed");
   result.classList.add("success");
   noteStreak += 1;
+  failingStreak = 0;
+  setTimeout(play_random_note, 1000);
 }
 
 function wrong_note(){
+  failingStreak += 1;
   var result = document.getElementById("result");
   result.innerHTML = "Errado!";
   result.classList.remove("success");
